0062e11cddef9b856ce891277f1af699db53accd
SeanEng <65707608+SeanEng@users.noreply.github.com>
2020-06-27 21:49:59 +0800

    enter

--- backend/server.js
+++ backend/server.js
@@ -30,3 +30,14 @@ app.listen(port, () => {
 
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
